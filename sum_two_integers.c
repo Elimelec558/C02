67365fb35aa493445ab93dafdaeb8a99c9b38d75
Elimelec558 <tgnemmanuel@gmail.com>
2025-05-22 04:21:52 +0000

    hdh

--- sum_two_integers.c
+++ sum_two_integers.c
@@ -1,9 +1,9 @@
 //Programme demandant à l'utilisateur de saisir deux entiers, puis affiche leur somme.
 #include<stdio.h>
 int main(){
-    int nombre1,nombre2;
+    int nombre1, nombre2;
     printf("Veuillez entrer deux entiers de votre choix:\n");
-    scanf("%d %d",&nombre1,&nombre2);
-    int somme=nombre1+nombre2;
-    printf("La somme est : %d",somme);
+    scanf("%d %d", &nombre1, &nombre2);
+    int somme = nombre1 + nombre2;
+    printf("La somme est : %d\n",somme);
     }
